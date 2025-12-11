3b6eb7b6913af4af112be852d8620dd4661a064b
Allan <nabuleallan1@gmail.com>
2025-12-11 12:18:26 +0300

    added import function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -130,3 +130,15 @@ const importBtn = document.getElementById('importBtn');
 importBtn.addEventListener('click', () => {
     importFromJsonFile();
 });
+
+function createDowlnloadLink(data, filename) {
+    const blob = new Blob([data], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
